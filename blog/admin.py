322b1b8395dde43290fcf898bdda8b2f45d9f59f
at4260 <at4260@gmail.com>
2015-05-13 14:46:36 -0700

    Initializing DjangoGirlsApp

---
+++ blog/admin.py
@@ -0,0 +1,10 @@
+# Register your models here.
+
+from django.contrib import admin
+from .models import Post
+
+# class PostAdmin(admin.ModelAdmin):
+#     pass
+
+# admin.site.register(Post, PostAdmin)
+admin.site.register(Post)
